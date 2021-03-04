1225894ba3e93fa62a3691fd247edf2a60a84402
Mark Makuno <makunomark@gmail.com>
2021-03-04 13:02:18 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '421f473182e3137a9045f1741cf917cc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c68c2c6e-39d5-4271-a648-cf3b59ece287'
+};
